4df127697a24a7ad3fed35b24461dc65cd7f1c63
maddccr <ccr.maddie@gmail.com>
2023-08-10 22:58:04 -0300

    Events!

---
+++ eventos.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function(){
+    const divbtn = document.getElementById("divbtn");
+    divbtn.addEventListener("click", function(){
+        alert("Hola! Soy el div")
+    });
+});
