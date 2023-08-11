5798db794a0ef41f6463e5ff8ee7f5d108a32398
maddccr <ccr.maddie@gmail.com>
2023-08-10 23:01:43 -0300

    Usando defer

--- eventos.js
+++ eventos.js
@@ -1,6 +1,4 @@
-document.addEventListener("DOMContentLoaded", function(){
-    const divbtn = document.getElementById("divbtn");
-    divbtn.addEventListener("click", function(){
-        alert("Hola! Soy el div")
-    });
+const divbtn = document.getElementById("divbtn");
+divbtn.addEventListener("click", function(){
+    alert("Hola! Soy el div")
 });
